34bbbc3f688604312117dadd454c1f1e5155c80d
Johan Swan <jswannieman@gmail.com>
2024-01-02 09:46:38 +1300

    Update ColorPicker JSX Component

--- src/Color.jsx
+++ src/Color.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Color = ({ hex, description, setBackgroundColor }) => {
   return (
     <div
-      style={{ backgroundColor: hex, padding: "1rem" }}
+      style={{
+        backgroundColor: hex,
+        padding: "1rem",
+        border: "2px solid #FFF",
+      }}
       onClick={() => setBackgroundColor(hex)}
     >
       <h2 id={description}>{description}</h2>
